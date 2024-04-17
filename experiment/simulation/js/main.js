c7077b9bb1b1ba749c6b367bbc6ce06e6cbe4638
Nihal saran das duggirala <92871743+nihalsaran@users.noreply.github.com>
2024-04-17 16:06:44 +0530

    Main Page done

--- experiment/simulation/js/main.js
+++ experiment/simulation/js/main.js
@@ -1,1 +1,3 @@
-//Your JavaScript goes in here
+document.getElementById('nextToStep1').addEventListener('click', function() {
+    window.location.href = 'letterpage.html'; // Change 'step2.html' to the actual path of your next page if different
+});
