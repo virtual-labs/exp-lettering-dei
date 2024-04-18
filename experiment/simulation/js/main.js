5bc314c23da5421946fdd32413a7dca8a1daffe8
Nihal saran das duggirala <92871743+nihalsaran@users.noreply.github.com>
2024-04-18 14:09:46 +0530

    Template configured

--- experiment/simulation/js/main.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('nextToStep1').addEventListener('click', function() {
-    window.location.href = 'letterpage.html'; // Change 'step2.html' to the actual path of your next page if different
-});
